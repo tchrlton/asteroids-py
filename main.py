d153f747bd997ab7e496df522cd5b529fe81fef9
tchrlton <tchrlton1@gmail.com>
2025-03-21 23:36:55 -0700

    add player movement and grouping

--- main.py
+++ main.py
@@ -1,21 +1,41 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
   pygame.init()
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+  clock = pygame.time.Clock()
+  dt = 0
+  
   print("Starting Asteroids!")
   print(f"Screen width: {SCREEN_WIDTH}")
   print(f"Screen height: {SCREEN_HEIGHT}")
+  
+  updatable = pygame.sprite.Group()
+  drawable = pygame.sprite.Group()
+  
+  Player.containers = (updatable, drawable)
+  
+  player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
   
   while True:
     for event in pygame.event.get():
       
       if event.type == pygame.QUIT:
+        
           return
       
     screen.fill("black")
+    
+    for item in drawable:
+      item.draw(screen)
+    
     pygame.display.flip()
+    dt = clock.tick(60) / 1000
+    
+    updatable.update(dt)
+    
 
 if __name__ == "__main__":
     main()
