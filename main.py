2f06262c251fba76ec32938f85137c926318a43d
tchrlton <tchrlton1@gmail.com>
2025-03-22 16:41:24 -0700

    fix player rotation and add asteroids and field

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
   pygame.init()
@@ -12,12 +14,16 @@ def main():
   print(f"Screen width: {SCREEN_WIDTH}")
   print(f"Screen height: {SCREEN_HEIGHT}")
   
+  asteroids = pygame.sprite.Group()
   updatable = pygame.sprite.Group()
   drawable = pygame.sprite.Group()
   
   Player.containers = (updatable, drawable)
+  Asteroid.containers = (asteroids, updatable, drawable)
+  AsteroidField.containers = (updatable)
   
   player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+  asteroidField = AsteroidField()
   
   while True:
     for event in pygame.event.get():
